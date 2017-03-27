492849851d0175aef2249ffe5b530f49c16a5b33
Artur Dudnik <adudnik@gmail.com>
2017-03-27 22:17:40 +0300

    extend syntax

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
-import Swagger from 'swagger-client'
+import Swagger from 'swagger-client';
 
-export default (opts) => store => next => {
-  var waitQueue = []
-  var ready = false
-
-  var callApi = (swagger) => {
-    if (typeof swagger === 'function')
-      swagger(client)
-    else
-      console.error("Swagger api call is not a function")
-  }
-
-  var client = new Swagger({
+export default (opts) => () => next => action => {
+  const { swagger, types, ...rest } = action; // eslint-disable-line no-redeclare
+  const [REQUEST, SUCCESS, FAILURE] = types;
+  const waitQueue = [];
+  let ready = false;
+  const callApi = client => sw => {
+    if (typeof swagger === 'function') {
+      sw(client)
+        .then(
+          (result) => next({ ...rest, result, type: SUCCESS }),
+          (error) => next({ ...rest, error, type: FAILURE })
+        ).catch(error => {
+          console && console.error && console.error('MIDDLEWARE ERROR:', error);
+          next({ ...rest, error, type: FAILURE });
+        });
+    } else {
+      console.error('Swagger api call is not a function');
+    }
+  };
+  const client = new Swagger({
     ...opts,
     success: () => {
       ready = true
       while (waitQueue.length) {
-        var action = waitQueue.shift()
-        callApi(action.swagger)
-        next(action)
+        const a = waitQueue.shift();
+        callApi(client)(a.swagger);
+        next({ ...rest, type: REQUEST });
       }
     }
   });
-  return action => {
-    if (action.swagger) {
-      // Add async api calls to queue if not ready
-      if (ready == false)
-        waitQueue.push(action)
-      else {
-        // Call payload and pass the swagger client
-        callApi(action.swagger)
-        return next(action)
-      }
-    } else 
-      return next(action)
+
+  if (action.swagger) {
+    // Add async api calls to queue if not ready
+    if (!ready) {
+      waitQueue.push(action);
+    } else {
+      // Call payload and pass the swagger client
+      callApi(client)(action.swagger);
+      return next(action);
+    }
+  } else {
+    return next(action);
   }
+  return false;
 }
